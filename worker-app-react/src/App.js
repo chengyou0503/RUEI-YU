027e6eaba4946f10688c2a118ffca2c2047149a6
chengyou0503 <99228903+chengyou0503@users.noreply.github.com>
2025-11-11 23:27:16 +0800

    feat: 更新 Google Apps Script 部署 URL

--- worker-app-react/src/App.js
+++ worker-app-react/src/App.js
@@ -13,7 +13,7 @@ import ReturnSuccessPage from './components/ReturnSuccessPage';
 import ShoppingCart from './components/ShoppingCart';
 import WorkLogPage from './components/WorkLogPage'; // **新增**
 
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwQ2r03QKViYEo7UY6PowM0h8lsEoRgukYdRfU1CIvwMEcCtUMLxLq4l1rAcVGgkzF8-A/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxDFGOMGQYEu5SooXsFoJsL733Tn3eI_ioaJUSUFcXmJE7Av9N31TYszUeq8QnBjOsasQ/exec";
 const requestSteps = ['身份驗證', '主選單', '專案資訊', '選擇品項', '預覽與確認'];
 const returnSteps = ['身份驗證', '主選單', '退貨申請'];
 const logSteps = ['身份驗證', '主選單', '填寫日誌']; // **新增**
