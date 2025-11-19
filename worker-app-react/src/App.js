1186cb2caab7920d38add1fa055f5e142ab39773
chengyou0503 <99228903+chengyou0503@users.noreply.github.com>
2025-11-19 10:35:13 +0800

    chore: 更新後端 GAS URL

--- worker-app-react/src/App.js
+++ worker-app-react/src/App.js
@@ -13,7 +13,7 @@ import ReturnSuccessPage from './components/ReturnSuccessPage';
 import ShoppingCart from './components/ShoppingCart';
 import WorkLogPage from './components/WorkLogPage'; // **新增**
 
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxLtKEVSZ744UqkrazxG6BRP6CLQJiWOYXWYp6PG9uqFq99hQfaR9p0-rLubSLM-HS2/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzeEXh_qZb4VNWiq7XCZsEjNxZvi87V9ckcKM0FwcAEQ0_mdjgkQv63MDF-Vp4X2Tn4GQ/exec";
 const requestSteps = ['身份驗證', '主選單', '專案資訊', '選擇品項', '預覽與確認'];
 const returnSteps = ['身份驗證', '主選單', '退貨申請'];
 const logSteps = ['身份驗證', '主選單', '填寫日誌']; // **新增**
